4d4bf707cee6d90c8f0f26422e6d1c0784bb9d18
Fred Wu <xuetianw@sfu.ca>
2018-10-15 01:09:11 -0700

    finish pwReduce function

--- Ass1/main.hs
+++ Ass1/main.hs
@@ -1,7 +1,21 @@
 
 import RainbowAssign
-main :: IO ()
-main = do
-  generateTable
-  res <- test2 10000
-  print res
+-- main :: IO ()
+-- main = do
+--   generateTable
+--   res <- test2 10000
+--   print res
+pwLength, nLetters, width, height :: Int
+filename :: FilePath
+pwLength = 8            -- length of each password
+nLetters = 5            -- number of letters to use in passwords: 5 -> a-e
+width = 40              -- length of each chain in the table
+height = 1000           -- number of "rows" in the table
+filename = "table.txt"  -- filename to store the table
+
+
+convertToBase :: Int -> String
+convertToBase a = [toLetter (mod a nLetters)] ++ convertToBase(div a nLetters)
+
+pwReduce :: Int -> String
+pwReduce a = reverse(take pwLength (convertToBase a))
