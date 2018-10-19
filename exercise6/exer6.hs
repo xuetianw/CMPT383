76552ade00e01ed1b1cff23ff7c816b51d534f34
Fred Wu <xuetianw@sfu.ca>
2018-10-19 05:51:08 -0700

    finish All Rational Numbers

--- exercise6/exer6.hs
+++ exercise6/exer6.hs
@@ -8,3 +8,9 @@ rationalSum a = [ (x % y) | x <- [1..a], y <- [1..a], y == (a - x)]
 
 rationalSumLowest :: (Integral a) => a -> [Ratio a]
 rationalSumLowest a = [ (x % y) | x <- [1..a], y <- [1..a], y == (a - x), gcd x y ==1]
+
+-- All Rational Numbers
+rationals :: (Integral a) => [Ratio a]
+rationals = rationalSumLowestRec 0
+ where 
+ 	rationalSumLowestRec a = (rationalSumLowest a) ++ (rationalSumLowestRec (a+1))
