daed4751558626c0132283a78396993b8f7fceb8
xuetianw <*>
2018-10-18 17:21:45 -0700

    correct file name

--- exercise6/exer6.hs
+++ exercise6/exer6.hs
@@ -1,4 +1,6 @@
 -- Rational Numbers
 import Data.Ratio
-rationalSumHelper :: (Integral a) => a -> [Ratio a]
-rationalSumHelper a = [ (x % y) | x <- [1..a], y <- [1..a], y == (a - x)]
+rationalSum :: (Integral a) => a -> [Ratio a]
+rationalSum a = [ (x % y) | x <- [1..a], y <- [1..a], y == (a - x)]
+
+--All Rational Numbers
