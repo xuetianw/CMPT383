fb2dbc10b6d7380696387f1bc90d2d3a75cf871d
xuetianw <*>
2018-10-19 02:30:12 -0700

    Lowest Terms Only

--- exercise6/exer6.hs
+++ exercise6/exer6.hs
@@ -1,6 +1,10 @@
 -- Rational Numbers
 import Data.Ratio
+
 rationalSum :: (Integral a) => a -> [Ratio a]
 rationalSum a = [ (x % y) | x <- [1..a], y <- [1..a], y == (a - x)]
 
---All Rational Numbers
+-- Lowest Terms Only
+
+rationalSumLowest :: (Integral a) => a -> [Ratio a]
+rationalSumLowest a = [ (x % y) | x <- [1..a], y <- [1..a], y == (a - x), gcd x y ==1]
