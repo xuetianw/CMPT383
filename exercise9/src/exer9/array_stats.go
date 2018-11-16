ad1bb49ff8ed2c9ddcd9e924f6b1ba3a3d494b97
Fred Wu <xuetianw@sfu.ca>
2018-11-16 08:29:04 -0800

    partial complete exercise 9

--- exercise9/src/exer9/array_stats.go
+++ exercise9/src/exer9/array_stats.go
@@ -1,9 +1,15 @@
 package exer9
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
+
+type Sum struct {
+	part1 int
+	part2 int
+}
 
 func RandomArray(length int, maxInt int) []int {
 	// TODO: create a new random generator with a decent seed; create an array with length values from 0 to values-1.
@@ -17,11 +23,49 @@ func RandomArray(length int, maxInt int) []int {
 
 }
 
-//func MeanStddev(arr []int, chunks int) (mean, stddev float64) {
-//	if len(arr)%chunks != 0 {
-//		panic("You promised that chunks would divide slice size!")
-//	}
-//	// TODO: calculate the mean and population standard deviation of the array, breaking the array into chunks segments
-//	// and calculating on them in parallel.
-//
-//}
+func MeanStddev(arr []int, chunks int) (mean, stddev float64) {
+	if len(arr)%chunks != 0 {
+		panic("You promised that chunks would divide slice size!")
+	}
+	// TODO: calculate the mean and population standard deviation of the array, breaking the array into chunks segments
+	// and calculating on them in parallel.
+
+	var ch = make(chan Sum)
+
+	size := len(arr) / chunks
+	for i := 0; i < len(arr); i += size {
+		temp := arr[i : i+size]
+		go calculation(temp, ch)
+	}
+	close(ch)
+
+	var sum1 int
+	var squrSum int
+
+	for item := range ch {
+		sum1 += item.part1
+		squrSum += item.part2
+	}
+
+	mean = float64(sum1 / len(arr))
+
+	part1 := (squrSum) / len(arr)
+	part2 := sum1 / len(arr)
+
+	stddev = math.Sqrt(float64(part1 - part2))
+
+	return
+
+}
+
+func calculation(ints []int, sums chan Sum) {
+	var sum int
+	for i := 0; i < len(ints); i++ {
+		sum += ints[i]
+	}
+	var sumSqu int
+	for i := 0; i < len(ints); i++ {
+		sumSqu += (ints[i]) * (ints[i])
+	}
+	sums <- Sum{sum, sumSqu}
+}
