c4edb52ff40f0c31f79ca852d9786376dabaa781
Fred Wu <xuetianw@sfu.ca>
2018-11-23 12:31:18 -0800

    fib fix

--- exercise10/fibonacci.go
+++ exercise10/fibonacci.go
@@ -1,27 +1,32 @@
-package main
-
-import "fmt"
+package exercise10
 
 func Fib(n uint, cutoff uint) uint {
-	var ch = make(chan uint, 1)
-	FibCocurrent(n, ch, cutoff)
-	return <-ch
+	if n > cutoff {
+		var ch = make(chan uint, 1)
+		FibCocurrent(n, ch)
+		return <-ch
+	} else {
+		return FibSequential(n)
+	}
 }
 
-func FibCocurrent(u uint, ch chan uint, cutoff uint) {
+func FibSequential(u uint) uint {
+	if u == 0 || u == 1 {
+		return 1
+	} else {
+		return FibSequential(u-1) + FibSequential(u-2)
+	}
+}
+
+func FibCocurrent(u uint, ch chan uint) {
 	if u == 0 || u == 1 {
 		ch <- 1
 		return
 	}
 	leftChan := make(chan uint, 1)
 	rightChan := make(chan uint, 1)
-	if (cutoff > 0) {
-		go FibCocurrent(u - 1, leftChan, cutoff - 1)
-		go FibCocurrent(u - 2, rightChan, cutoff -1)
-	} else {
-		FibCocurrent(u - 1, leftChan, cutoff - 1)
-		FibCocurrent(u - 2, rightChan, cutoff - 1)
-	}
+	go FibCocurrent(u-1, leftChan)
+	go FibCocurrent(u-2, rightChan)
 	left := <-leftChan
 	right := <-rightChan
 	ch <- left + right
@@ -30,9 +35,10 @@ func FibCocurrent(u uint, ch chan uint, cutoff uint) {
 }
 
 func Fibonacci(n uint) uint {
-	return Fib(n, 1000)
+	return Fib(n, 30)
 }
 
-func main() {
-	fmt.Print(Fibonacci(40))
-}
+//
+//func main() {
+//	fmt.Print(Fibonacci(30))
+//}
